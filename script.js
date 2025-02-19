00979679a1eb4ac350371fb3ea42b64d0e6244e3
Sam Bain <swb54@uclive.ac.nz>
2025-02-19 22:33:46 +1300

    Made project tabs flexible, added background blur on popup

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openPopup(id) {
     document.getElementById(id).style.display = "block";
+    document.getElementById("content").classList.add("popup-active"); // Add blur effect
 }
 
 function closePopup(id) {
     document.getElementById(id).style.display = "none";
+    document.getElementById("content").classList.remove("popup-active"); // Remove blur effect
 }
