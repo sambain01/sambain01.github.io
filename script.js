d493811ddd3c228c7d9d15a3e3b7449beba612a3
Sam Bain <swb54@uclive.ac.nz>
2025-02-21 21:46:05 +1300

    Added new visualise photos and content for spray drone

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function openPopup(id) {
-    document.getElementById(id).style.display = "block";
+    let popup = document.getElementById(id);
+    popup.style.display = "block";
     document.getElementById("content").classList.add("popup-active"); // Add blur effect
+
+    // Ensure it starts at the top
+    popup.querySelector('.popup-content').scrollTop = 0;
 }
 
 function closePopup(id) {
     document.getElementById(id).style.display = "none";
     document.getElementById("content").classList.remove("popup-active"); // Remove blur effect
 }
+
+// Close pop-up when clicking outside the content
+
+var pop_up_open = false;
+addEventListener("click", function (event) {
+    document.querySelectorAll(".popup").forEach(popup => {
+        let content = popup.querySelector(".popup-content");
+
+        //If the click is outside the content, close the pop-up
+        if (popup.style.display == "block" && !content.contains(event.target)) {
+            if (pop_up_open) {
+                closePopup(popup.id);    
+            }
+            pop_up_open = !pop_up_open;
+        }
+    });
+});
